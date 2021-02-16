f1676d112e9cacc5bf18f2f9493d5522c69a8128
jamescross <crossjames813@gmail.com>
2021-02-16 16:27:21 -0600

    commit tree b1cd5dee2b140e15c91ccb2a5b179a79466946b9

--- grandma.rb
+++ grandma.rb
@@ -1,9 +1,10 @@
-def speak_to_grandma (string)
-    if string == "I love you grandma!".upcase
-        return "I love you too Pumpkin!".upcase
-    elsif string == string.upcase
-        return "NO, NOT SINCE 1938!"
+def speak_to_grandma(phrase)
+    if phrase == "I LOVE YOU GRANDMA!"
+        "I LOVE YOU TOO PUMPKIN!"
+    elsif phrase == phrase.upcase
+        "NO, NOT SINCE 1938!"
     else
-        return "Huh?! speak up, Sonny!".upcase
+        "HUH?! SPEAK UP, SONNY!"
     end
 end
+
